95f433886f3f97a5e91d68f4695a0245ee8a9ff2
puromed <arfanhaziq33@gmail.com>
2024-12-11 19:50:07 +0800

    Add UsersSeed with conditional seeding

---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+use Cake\Auth\DefaultPasswordHasher;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'email' => 'test123@localhost.com',
+                'password' => $hasher->hash('test123'),
+                'role' => 'admin',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'email' => 'cuba@localhost.com',
+                'password' => $hasher->hash('cuba'),
+                'role' => 'user',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+        ];
+        $table = $this->table('users');
+        $table->truncate(); //clear existing data before inserting
+        $table->insert($data)->save();
+    }
+}
